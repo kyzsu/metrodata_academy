4fbbbed933454ea39304934da3a24663dbefd89a
William Irsal <immanuelirsal1@gmail.com>
2020-09-24 13:24:57 +0700

    Icons and Header added

--- app/src/organisms/mainnav/index.jsx
+++ app/src/organisms/mainnav/index.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import NavItemIcon from "../../atoms/nav-item-icon";
+import { Dropdown } from "react-bootstrap";
 import NavItemText from "../../atoms/nav-item-text";
 
 const Mainnav = () => {
   return (
     <nav className="main-header navbar navbar-expand navbar-white navbar-light">
       <ul className="navbar-nav">
-        <NavItemIcon href="#" iconClass="fas fa-bars" />
-        <NavItemText href="#" text="Home" />
-        <NavItemText href="#" text="Contact" />
+        <h3>
+          <NavItemText href="#" text="Star Wars API" />
+        </h3>
+      </ul>
+      <ul className="navbar-nav ml-auto">
+        <Dropdown>
+          <Dropdown.Toggle variant="transparant" id="dropdown-basic">
+            <img
+              src="https://image.flaticon.com/icons/png/512/126/126472.png"
+              alt="setting"
+              width="25px"
+              height="25px"
+            />
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Reset Password</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Logout</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </ul>
     </nav>
   );
